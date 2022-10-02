26d317a03c94de590a81d75f58c3219638b3faaf
isiagi <isiagigeofrey0@gmail.com>
2022-10-02 16:19:54 +0300

    add initial routing

---
+++ src/pages/Home.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <h1>WittyLoop</h1>
+      </div>
+      <div>
+        <p>
+          WittyLoop is a technology company that deals in software development,
+          data and analytics, Artificial Intelligence.
+        </p>
+      </div>
+      <div>
+        <h2>COMING UP SOON</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
