7af010f041f77757ad7c6a9e43c428a9db28ea87
isiagi <isiagigeofrey0@gmail.com>
2022-10-13 12:47:06 +0300

    adds footer

---
+++ src/components/footer/Last.tsx
@@ -0,0 +1,13 @@
+import "./footer.css";
+
+function Last() {
+  return (
+    <div className="last__container">
+        <div className="last__wrapper">
+            WittyLoop @ 2022
+        </div>
+    </div>
+  )
+}
+
+export default Last
